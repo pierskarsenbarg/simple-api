2277fbbf75bb78f6819ab7f66a14de5e73a8e5e2
Piers Karsenbarg <piers@pulumi.com>
2023-07-20 12:21:04 +0100

    updated output of lambda

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@ exports.handler = async (e) => {
     console.log(e);
     return {
         statusCode: 200,
-        body: JSON.stringify({message: "Hello, World!"})
+        body: JSON.stringify({message: "Hello, World from Pulumi!"})
     };
 }
